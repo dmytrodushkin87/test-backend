ec3e48f0d29624529ade7d38605c5e3f3fac3c12
dmytrodushkin87 <dmytrodushkin87@gmail.com>
2022-05-02 23:25:05 +0300

    Feature: Test task

---
+++ src/database/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'swift'
+});
+
+
+module.exports = connection;
